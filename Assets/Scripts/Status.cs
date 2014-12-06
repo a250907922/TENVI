a85243ead3943b4d7c1cb2c7df19b4ec0dc11b45
chocopun <b1012181@Project20-no-MacBook-Air-2.local>
2014-12-07 07:15:36 +0900

    edit status.cs

--- Assets/Scripts/Status.cs
+++ Assets/Scripts/Status.cs
@@ -3,25 +3,42 @@ using System.Collections;
 
 public class Status : MonoBehaviour {
 
-    public int  powerLevel;
+    public int playerLevel;
+    public int hpLevel;
+    public int powerLevel;
+    public int defenceLevel;
+    private float statusWidth;
+    private float statusHeight;
+
 
     void Awake() {
+        playerLevel = PlayerPrefs.GetInt("playerLevel");
+        playerLevel = PlayerPrefs.GetInt("hpLevel");
         powerLevel = PlayerPrefs.GetInt("powerLevel");
+        playerLevel = PlayerPrefs.GetInt("defenceLevel");
+    }
+
+    void Start() {
+        statusWidth = 200.0f;
+        statusHeight = 280.0f;
     }
 
     void OnGUI () {
         /* GUIグループの生成*/
-        GUI.BeginGroup (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 70, 100, 140));
-        GUI.Box (new Rect (0,0,100,140), "Power");
-        if(GUI.Button (new Rect (10,30,80,30), "Level up")){
+        GUI.BeginGroup (new Rect ((Screen.width/2) - (statusWidth/2), (Screen.height/2) - (statusHeight/2), statusWidth, statusWidth)); // 真ん中に100*140のグループ
+        GUI.Box (new Rect (0,0, statusWidth, statusWidth), "Player Level");
+
+        /* Power */
+        GUI.Label (new Rect (0,statusHeight/5,100,30), "Power");
+        if(GUI.Button (new Rect (statusWidth/3,statusHeight/5,100,30), "Level Up")){
             powerLevel++;
             PlayerPrefs.SetInt("powerLevel", powerLevel);
         }
-        if (GUI.Button (new Rect(10, 70, 80, 30), "Reset")) {
+        if (GUI.Button (new Rect(statusWidth*2/3, statusHeight/5, 100, 30), "Reset")) {
             powerLevel = 0;
             PlayerPrefs.DeleteKey("powerLevel");
         }
-        GUI.Box (new Rect (10,120,80,30), powerLevel.ToString());
+        GUI.Label (new Rect (10,120,80,30), powerLevel.ToString());
         GUI.EndGroup ();
         /* GUIグループ終了*/
     }
